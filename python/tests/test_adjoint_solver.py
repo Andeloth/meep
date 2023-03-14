deecdac31e80c7df32db63f055add77904b23dd7
Andeloth <benszthehun@gmail.com>
2023-03-14 09:39:09 -0600

    Added previous work done by Alex Kaylor

--- python/tests/test_adjoint_solver.py
+++ python/tests/test_adjoint_solver.py
@@ -624,6 +624,41 @@ class TestAdjointSolver(ApproxComparisonTestCase):
                 f"adjoint gradient={adj_dd}, finite difference={fnd_dd}"
             )
 
+    def test_Poynting_Flux(self):
+        print("*** TESTING POYNTING OBJECTIVE ***")
+
+        # test the single frequency and multi frequency cases
+        for frequencies in [[self.fcen], [1 / 1.58, self.fcen, 1 / 1.53]]:
+            # compute objective value and its gradient for unperturbed design
+            unperturbed_val, unperturbed_grad = self.adjoint_solver(
+                self.p, MonitorObject.POYNTING, frequencies, need_gradient=False
+            )
+
+            # compute objective value for perturbed design
+            perturbed_val = self.adjoint_solver(
+                self.p + self.dp,
+                MonitorObject.POYNTING,
+                frequencies,
+                need_gradient=False,
+            )
+
+            # compare directional derivative
+            if unperturbed_grad.ndim < 2:
+                unperturbed_grad = np.expand_dims(unperturbed_grad, axis=1)
+            adj_dd = (self.dp[None, :] @ unperturbed_grad).flatten()
+            fnd_dd = perturbed_val - unperturbed_val
+            print(
+                f"directional derivative:, {adj_dd} (adjoint solver), {fnd_dd} (finite difference)"
+            )
+            print("\n\n\n\n\n")
+            print("")
+            print("This is the unperturbed poynting gradient:")
+            print(unperturbed_grad)
+
+            tol = 0.07 if mp.is_single_precision() else 0.006
+            self.assertClose(adj_dd, fnd_dd, epsilon=tol)
+
+
     def test_gradient_backpropagation(self):
         """Verifies that the adjoint gradient can be back propagated through
         a differentiable mapping function applied to the design region
